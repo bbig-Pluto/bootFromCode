1fd77493921920090c32d1d6e3804c230abbcd82
bbig-Pluto <xkdxkd011@gmail.com>
2023-01-03 09:51:42 +0900

    init_All

---
+++ ex2Re/src/test/java/org/zerock/ex2Re/Ex2ReApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.ex2Re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2ReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
